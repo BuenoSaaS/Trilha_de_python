494c19dc54489b6088afdc65a78fabd77e7c8649
BuenoSaaS <rafaelmoussalle@gmail.com>
2026-05-17 15:56:39 -0300

    feat: Estrutura de exibição dos resultados ao usuário, junto de explicitar quanto sobrará ou faltara para a viagem... fix: Ajeitados quesitos de estrutura geral e prints dos resultados

--- Calculadora_viagem.py
+++ Calculadora_viagem.py
@@ -1,4 +1,4 @@
-while True: ## Loop para garantir que o usuário insira valores válidos
+while True: ## Loop para garantir que o usuário insira valores válidos e para caso haja algum erro na inserção dos dados
   try:
     Orcamento_disponivel = float(input("Insira o valor disponível para a viagem ")) 
     Destino = input("Qual o país ou cidade desejado? ")
@@ -10,10 +10,11 @@ while True: ## Loop para garantir que o usuário insira valores válidos
       break
     else:
       print("Valores inválidos, insira apenas positivos para os referentes a dinheiro e à quantidade de dias ou palavras para o destino ")
-      break
+      continue
 
   except ValueError:
     print("Insira valores válidos para os dados requisitados ")
+    continue
 
 
 Valor_euro = 5.9
@@ -23,16 +24,27 @@ Custo_hospedagem_reais = Valor_hospedagem_reais * Duracao_viagem ## Cálculo de 
 Custo_total_viagem = Custo_passagem + Custo_hospedagem_reais ##Cálculo do custo total da viagem
 
 if Custo_total_viagem <= Orcamento_disponivel: ##Condicional que define se o orçamento é suficiente dado custo
-  situacao = "Orçamento suficiente"
-else:  situacao = "Orçamento insuficiente"
+  situacao = "suficiente"
+else:  situacao = "insuficiente"
 
 Viagem_viavel = (Custo_total_viagem <= Orcamento_disponivel) and Duracao_viagem > 0 ##Verificação de viabilidade
 
 if Viagem_viavel:
-  resultado = "Viagem possível"
-else:  resultado = "Viagem não possível"
+  resultado = "viagem é possível"
+else:  
+  resultado = "viagem não é possível"
 
 Sobra_ou_Precisa = Orcamento_disponivel - Custo_total_viagem ##Cálculo de sobra ou valor necessário para realizar a viagem
 
 
+print ("\nResultados:")   ##Exibição dos resultados para o usuário 
 
+print(f"O valor total gasto com a hospedagem é de R$ {Custo_hospedagem_reais:.2f}")
+print(f"O custo total da viagem é de R$ {Custo_total_viagem:.2f}")
+print(f"O orçamento para a viagem é {situacao}")
+print(f"Então, com base nos resultados prévios a {resultado}")
+
+if Sobra_ou_Precisa > 0:  ##Condicional para exibir se haverá sobra ou se precisará de mais dinheiro para realizar a viagem
+    print(f"Você terá uma sobra de R$ {Sobra_ou_Precisa:.2f} após a viagem. Arrume suas malas e aproveite sua viagem para {Destino}!")
+else:    
+  print(f"Você precisará juntar R$ {abs(Sobra_ou_Precisa):.2f} adicionais para realizar a viagem.")
